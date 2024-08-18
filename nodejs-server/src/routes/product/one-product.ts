4af3b8861f7c6dfa44386021849c259a8742eb9d
Win Tun <wintun.hlaing@outlook.com>
2024-08-18 13:09:31 +0630

    crud test

--- nodejs-server/src/routes/product/one-product.ts
+++ nodejs-server/src/routes/product/one-product.ts
@@ -4,6 +4,7 @@ import { ProductRepo } from '../../repositories/product-repo';
 // declare route
 export const oneProductRoute = Router();
 
+// Create(C)
 oneProductRoute.post("/",async (req:Request,res:Response) => {
     try {
         const reqBody = req.body
@@ -14,7 +15,7 @@ oneProductRoute.post("/",async (req:Request,res:Response) => {
     }
 })
 
-
+// Read One Prodct by Id(R)
 oneProductRoute.get('/:id',async (req:Request,res:Response) => {
     try {
         const {id} = req.params;
@@ -25,6 +26,42 @@ oneProductRoute.get('/:id',async (req:Request,res:Response) => {
             res.status(400).json({"message":"Not Found!"})
         }
     } catch (e) {
+        console.log("error",e);
+        res.status(500).json({"message":"Internal server error!"})
+    }
+});
+
+// update one product By id(U)
+oneProductRoute.put('/:id',async (req:Request,res:Response) => {
+    try {
+        const {id} = req.params;
+        const reqBody = req.body
+        const search = await ProductRepo.selectById(id)
+        if (search) {
+            const updateData = {...search,...reqBody,updatedAt:new Date().toISOString()}
+            const result = await ProductRepo.update(id,updateData)
+            res.status(200).json({"message":"success",result:result});
+        }else{
+            res.status(400).json({"message":"Not Found or Already deleted!"})
+        } 
+    } catch (e) {
+        console.log("error",e);
+        res.status(500).json({"message":"Internal server error!"})
+    }
+})
+
+// delete one product by id(D)
+oneProductRoute.delete("/:id",async (req:Request, res:Response) => {
+    try{
+        const {id} = req.params;
+        const search = await ProductRepo.selectById(id)
+        if (search) {
+            const result = await ProductRepo.remove(id)
+            res.status(200).json({"message":"success",result:result});
+        }else{
+            res.status(400).json({"message":"Not Found or Already deleted!"})
+        } 
+    }catch(e){
         console.log("error",e);
         res.status(500).json({"message":"Internal server error!"})
     }
