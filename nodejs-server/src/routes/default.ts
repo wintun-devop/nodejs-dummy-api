6f6e5bd991e5470140a42e6ee255af56db134cce
wintun-devop <wintun.hlaing@outlook.com>
2025-02-07 20:15:32 +0630

    update model

--- nodejs-server/src/routes/default.ts
+++ nodejs-server/src/routes/default.ts
@@ -1,9 +1,10 @@
 import { Router,Request, Response } from 'express';
 
+
 //declare router
 export const defaultRoute = Router();
 
 // crate request and response
-defaultRoute.get('/', (req:Request, res:Response) => {
+defaultRoute.get('/', async (req:Request, res:Response) => {
   res.send({"message":"API is working ok"})
 })
